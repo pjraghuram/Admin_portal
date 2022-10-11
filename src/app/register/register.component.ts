b4874bfa9a9993881cbff00d1e2cd23a3a36830a
pjraghuram <penugondaraghuram@gamil.com>
2022-10-11 14:27:49 +0530

    newcommit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,14 +8,16 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+ 
   confirmpassword: string='none';
+  displayMsg: string='';
+  isAccountCreated: boolean=false;
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
   }
 
-  registerform = new FormGroup({
+  registerform: FormGroup = new FormGroup({
     Adminname: new FormControl("", 
     [Validators.required,Validators.minLength(3),Validators.pattern("[A-Za-z]+( [A-Za-z]+)*$")]),
     Email: new FormControl("", 
@@ -30,8 +32,7 @@ export class RegisterComponent implements OnInit {
     [Validators.required,Validators.minLength(5)]),
     Phone: new FormControl("",
     [Validators.required,Validators.maxLength(10),Validators.minLength(10),Validators.pattern("[0-9]*")]),
-    PAN: new FormControl("",
-    [Validators.required,Validators.minLength(12),Validators.maxLength(12),Validators.pattern("[A-Z]{5}[0-9]{4}[A-Z]{1}")]),
+    PAN: new FormControl("",),
     dateofbirth: new FormControl("",
     [])
   });
@@ -40,8 +41,29 @@ export class RegisterComponent implements OnInit {
     if(this.password.value==this.cpassword.value){
       console.log('Submitted');
       this.confirmpassword='none';
-      this.authService.registerUser().subscribe(res => {
-        console.log(res);
+      this.authService.registerUser(
+        [
+          this.registerform.value.Adminname,
+          this.registerform.value.Email,
+          this.registerform.value.Username,
+          this.registerform.value.Password,
+          this.registerform.value.Address,
+          this.registerform.value.Phone,
+          this.registerform.value.PAN,
+        ]
+      ).subscribe(res => {
+        if(res == 'Success'){
+          this.displayMsg = 'Account Created Successfully';
+          this.isAccountCreated=true;
+        }
+        else if(res == 'Already Registered'){
+          this.displayMsg='Account Already Exist. Use another Email.';
+          this.isAccountCreated=false;
+        }
+        else{
+          this.displayMsg='Something went wrong';
+          this.isAccountCreated=false;
+        }
       })
     }
     else{
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,8 +8,16 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
   baseServerUrl = "https://localhost:44359/api/";
-  registerUser(){
-    return this.http.post(this.baseServerUrl + "User/CreateUser",null,{
+  registerUser(user: Array<String>){
+    return this.http.post(this.baseServerUrl + "User/CreateUser",{
+      Name: user[0],
+      Email: user[1],
+      UniqueUsername: user[2],
+      Password: user[3],
+      Address: user[4],
+      PhoneNumber: user[5],
+      PAN: user[6]
+    },{
       responseType:'text',
     });
   }
